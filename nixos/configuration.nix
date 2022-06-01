fecc8e521fd02663b8f550c6ff64d82e1858a9fb
Michael Schoderer <schoderer@googlemail.com>
2022-06-01 21:10:18 +0200

    Switch to gnome

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -71,10 +71,10 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = false;
-  services.xserver.desktopManager.gnome.enable = false;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.sddm.enable = false;
+  services.xserver.desktopManager.plasma5.enable = false;
   # Configure keymap in X11
   services.xserver = {
     layout = "de";
